671c378c1f635df62ae83d00971378a0203fc665
adarshperi <adarshperi@gmail.com>
2019-06-16 13:32:48 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Signin from './Components/Signin';
+import Register from './Components/Register';
+import Navigation from './Components/Navigation';
+import Home from './Components/Home';
+import About from './Components/About';
 import './App.css';
 
-function App() {
+const particlesOptions = {
+
+                particles: {
+                  number: {
+                    value: 69,
+                    desnity: {
+                      enable:true,
+                      value_area:800
+                    }
+                  }
+              }
+            }
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 'home',
+      isSignedIn: false
+    }
+  }
+  
+  onRouteChange= (route1) => {
+    if(route1 === 'signout') {
+      this.setState({isSignedIn:false,route:'home'})
+    }
+    else if(route1 === 'home')
+    {
+      this.setState({isSignedIn: true,route:'home'})
+    }
+    else if(route1=== 'home1')
+    {
+      this.setState({isSignedIn:false, route:'home'})
+    }
+    else if(route1 === 'about1')
+    {
+      this.setState({isSignedIn: false,route: 'about'})
+    }
+    else if(route1 === 'about')
+    {
+      this.setState({isSignedIn: true,route: 'about'})
+    }
+    else {
+    this.setState({route: route1});
+    }
+   } 
+
+render(){
+  const {isSignedIn,route} = this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div className="App">
+       <Particles className='particles' params={particlesOptions}/>  
+      <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
+      { route === 'home' 
+       ?
+       <Home />
+       : (
+          route === 'signin'
+          ? <Signin  onRouteChange={this.onRouteChange}/>
+          : ( 
+              route ==='about' 
+              ? <About isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+              :<Register onRouteChange={this.onRouteChange} />
+        )
+          )
+      }
+   </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,176 @@
+import React, {Component} from 'react';
+import CanvasJSReact from '../canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+class Home extends Component {
+	constructor(){
+		super();
+	}
+
+	 render() {
+   		 const options = {
+   		 	theme: 'dark2',
+      title: {
+        text: "Column Chart"
+      },
+      data: [{				
+                type: "column",
+                dataPoints: [
+                    { label: "Apple",  y: 10  },
+                    { label: "Orange", y: 15  },
+                    { label: "Banana", y: 25  },
+                    { label: "Mango",  y: 30  },
+                    { label: "Grape",  y: 28  }
+                ]
+       }]
+   					}
+		 const options2 = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "dark2", // "light1", "dark1", "dark2"
+			title:{
+				text: "Bounce Rate by Week of Year"
+			},
+			axisY: {
+				title: "Bounce Rate",
+				includeZero: false,
+				suffix: "%"
+			},
+			axisX: {
+				title: "Week of Year",
+				prefix: "W",
+				interval: 2
+			},
+			data: [{
+				type: "line",
+				toolTipContent: "Week {x}: {y}%",
+				dataPoints: [
+					{ x: 1, y: 64 },
+					{ x: 2, y: 61 },
+					{ x: 3, y: 64 },
+					{ x: 4, y: 62 },
+					{ x: 5, y: 64 },
+					{ x: 6, y: 60 },
+					{ x: 7, y: 58 },
+					{ x: 8, y: 59 },
+					{ x: 9, y: 53 },
+					{ x: 10, y: 54 },
+					{ x: 11, y: 61 },
+					{ x: 12, y: 60 },
+					{ x: 13, y: 55 },
+					{ x: 14, y: 60 },
+					{ x: 15, y: 56 },
+					{ x: 16, y: 60 },
+					{ x: 17, y: 59.5 },
+					{ x: 18, y: 63 },
+					{ x: 19, y: 58 },
+					{ x: 20, y: 54 },
+					{ x: 21, y: 59 },
+					{ x: 22, y: 64 },
+					{ x: 23, y: 59 }
+				]
+			}]
+						}
+
+		 const options3 = {
+			theme: "dark2",
+			animationEnabled: true,
+			
+			exportEnabled: true,
+			title:{
+				text: "Customer Satisfaction"
+			},
+			data: [{
+				type: "pie",
+				showInLegend: true,
+				legendText: "{label}",
+				toolTipContent: "{label}: <strong>{y}%</strong>",
+				indexLabel: "{y}%",
+				indexLabelPlacement: "inside",
+				dataPoints: [
+					{ y: 8, label: "Very Unsatisfied" },
+					{ y: 24, label: "Unsatisfied" },
+					{ y: 38, label: "Neutral" },
+					{ y: 21, label: "Satisfied" },
+					{ y: 9, label: "Very Satisfied" },
+					
+				]
+			}]
+		}				
+   		return (
+     	 <div className='w-70 flex flex-column center'>
+     	 	<div className=' ma2'>
+        	<CanvasJSChart options = {options}/> 
+        	</div>
+        	<div className='  ma2'>
+        	<CanvasJSChart options = {options2} />
+        	</div>
+        	<div className=' ma2'>
+        	<CanvasJSChart options = {options3} />
+        	</div>
+      	</div>
+    );
+  }
+
+ 	// return(
+ 	// 	<section className="mw7 center avenir">
+ 	// 	<article className="bt bb b--black-10">
+ 	// 	<a className="db pv4 ph3 ph0-l no-underline black dim" href="#0">
+ 	// 	<div className="flex flex-column flex-row-ns">
+ 		
+ 	// 	<div className="w-100 w-60-ns pl3-ns">
+ 	// 	<h1 className="f3 fw1 baskerville mt0 lh-title">Tech Giant Invests Huge Money to Build a Computer Out of Science Fiction</h1>
+ 	// 	<p className="f6 f5-l lh-copy">
+ 	// 	The tech giant says it is ready to begin planning a quantum
+ 	// 	computer, a powerful cpu machine that relies on subatomic particles instead
+ 	// 	of transistors.
+ 	// 	</p>
+ 	// 	<p className="f6 lh-copy mv0">By Adarsh Peri</p>
+ 	// 	</div>
+ 	// 	</div>
+ 	// 	</a>
+ 	// 	<article className="bb b--black-10">
+ 	// 	<a className="db pv4 ph3 ph0-l no-underline black dim" href="#0">
+ 	// 	<div className="flex flex-column flex-row-ns">
+ 		
+ 	// 	<div className="w-100 w-60-ns pl3-ns">
+ 	// 	<h1 className="f3 fw1 baskerville mt0 lh-title">Giant Whale Invests Huge Money to Build a Computer Out of Plankton</h1>
+ 	// 	<p className="f6 f5-l lh-copy">
+ 	// 	Whale is the common name for a widely distributed and diverse
+ 	// 	group of fully aquatic placental marine mammals. They are an
+ 	// 	informal grouping within the infraorder Cetacea, usually
+ 	// 	excluding dolphins and porpoises.
+ 	// 	</p>
+ 	// 	<p className="f6 lh-copy mv0">By Adarsh Peri</p>
+ 	// 	</div>
+ 	// 	</div>
+ 	// 	</a>
+ 	// 	</article>
+ 	// 	</article>
+ 	// 	<article className="bb b--black-10">
+ 	// 	<a className="db pv4 ph3 ph0-l no-underline black dim" href="#0">
+ 	// 	<div className="flex flex-column flex-row-ns">
+ 		
+ 	// 	<div className="w-100 w-60-ns pl3-ns">
+ 	// 	<h1 className="f3 fw1 baskerville mt0 lh-title">Warehouse Prices Are Fast on the Rise</h1>
+ 	// 	<p className="f6 f5-l lh-copy">
+ 	// 	A warehouse is a commercial building for storage of goods.
+ 	// 	Warehouses are used by manufacturers, importers, exporters,
+ 	// 	wholesalers, transport businesses, customs, etc. They are
+ 	// 	usually large plain buildings in industrial areas of cities,
+ 	// 	towns and villages.
+ 	// 	</p>
+ 	// 	<p className="f6 lh-copy mv0">By Adarsh Peri</p>
+ 	// 	</div>
+ 	// 	</div>
+ 	// 	</a>
+ 	// 	</article>
+ 	// 	</section>
+
+ 	// 	)
+
+
+}
+
+export default Home;
